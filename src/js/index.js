01e5f6c80c92d76755854399d9987af45b6808b9
HalynaSilina <halynasilina@gmail.com>
2023-03-23 23:52:38 +0200

    functions

--- src/js/index.js
+++ src/js/index.js
@@ -8,9 +8,41 @@ const countryNameInput = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-countryNameInput.addEventListener('input', debounce(handleSearchCountryInput, DEBOUNCE_DELAY));
+countryNameInput.addEventListener(
+  'input',
+  debounce(handleSearchCountryInput, DEBOUNCE_DELAY)
+);
 
 function handleSearchCountryInput(e) {
-const countryName = e.target.value;
-console.log(countryName);
+  const countryName = e.target.value.trim();
+  fetchCountries(countryName)
+    .then(data => searchSpecifityCheck(data))
+    .catch(error => onError(error));
+}
+
+function searchSpecifityCheck(data) {
+  if (data.length > 10) {
+    return Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (data.length >= 2 && data.length <= 10) {
+    const markup = data
+      .map(
+        element =>
+          `<li><img src="${element.flags.svg}" width=60><h2>${element.name.official}</h2>`
+      )
+      .join('');
+    countryList.innerHTML = markup;
+  } else {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = `<img src="${data[0].flags.svg}" width=60><h2>${
+      data[0].name.official
+    }</h2><p>Capital: ${data[0].capital}</p><p>Population: ${
+      data[0].population
+    }</p><p>Languages: ${Object.values(data[0].languages)}</p>`;
+  }
+}
+function onError(error) {
+  console.error(error);
+  Notify.failure('Oops, there is no country with that name');
 }
