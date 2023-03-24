fc64350e29e755525d22673a42d7dd2ac8baf388
HalynaSilina <halynasilina@gmail.com>
2023-03-24 15:05:49 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,5 @@
-const BASE_URL = 'https://restcountries.com/v3.1/name'
-
 export function fetchCountries(name) {
-return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`).then(response=>{
+return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(response=>{
     if (!response.ok) {
         throw new Error(response.status);
     }
